4709e11f466b61771d83b2382b12e0fb6522259d
Sergey Shadrin <juuute3110@gmail.com>
2024-11-06 21:00:29 +0700

    first commit

---
+++ src/main/java/ru/shadrinsa/bookimori_api/BookimoriApiApplication.java
@@ -0,0 +1,13 @@
+package ru.shadrinsa.bookimori_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookimoriApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookimoriApiApplication.class, args);
+	}
+
+}
